cd3258e3882047d2536730b709769f3b70e6f070
praveen-raipuria <praveenraipuria@gmail.com>
2024-12-04 20:05:39 +0530

    initial comment

---
+++ src/test/java/SampleCheck/APITest001.java
@@ -0,0 +1,8 @@
+package SampleCheck;
+
+public class APITest001 {
+    public static void main(String[] args) {
+        System.out.println("API Testing");
+    }
+}
+
---
+++ src/test/java/SampleCheck/APITest002.java
@@ -0,0 +1,32 @@
+package SampleCheck;
+
+import io.restassured.RestAssured;
+
+public class APITest002 {
+    public static void main(String[] args) {
+
+        // Gherkins Syntax
+
+        // Full URL -> https://restful-booker.herokuapp.com/booking/1
+        // base URI -> https://restful-booker.herokuapp.com
+        // base Path -> /booking/1
+
+        RestAssured
+                .given()
+                .baseUri("https://restful-booker.herokuapp.com")
+                .basePath("/booking/1")
+                .when()
+                .get()
+                .then().log().all()
+                .statusCode(200);
+
+
+
+
+        // Expected status code <201> but was <200>.
+
+
+
+    }
+}
+
